14465bb5108bd6411da3f05d371d46851368898a
David Davis <david_davis@crowdstrike.com>
2018-10-04 13:09:43 -0400

    add Jinja2 pre-processing capabilities for yaml-based configs

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.0'
+version = '1.1.0'
 
 TESTS_REQUIRE = [
     'sparc.testing',
@@ -42,7 +42,8 @@ setup(name='sparc.config',
           'setuptools',
           'zope.interface',
           'zope.component',
-          'pyaml'
+          'pyaml',
+          'jinja2'
       ],
       tests_require=TESTS_REQUIRE,
       extras_require={
